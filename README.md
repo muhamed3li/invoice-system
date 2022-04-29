046a9061fa11adf2d9067ae6f92959c35612b3a4
muhamed3li <muhamedali.dev@gmail.com>
2022-04-29 04:31:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+##### How to install. 
+
+1- Download the project.
+
+2- Update .env file & create Database.
+
+3- Composer install.
+
+4- install packages (npm install && npm run dev)
+
---
+++ config/locale.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'status' => true,
+
+
+    'languages' => [
+
+        'en' => ['en', 'en_US', false, 'English'],
+        'ar' => ['ar', 'ar_EG', true, 'Arabic'],
+
+    ]
+
+];
